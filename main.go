3d3b50a62af717c7da3147b527de13162c527bec
Will Rouesnel <w.rouesnel@gmail.com>
2017-02-20 13:18:09 +1100

    Initial commit.
    TODOs:
    * Ensure command line options look right.
    * Add a Docker container build/push step.
    
    That should cover us for what we need which is a generic lorem generator process
    for demo uses.

---
+++ main.go
@@ -0,0 +1,42 @@
+// Test file for the lorem ipsum generator
+
+package main
+
+import (
+	"time"
+	lorem "github.com/drhodes/golorem"
+	cli "gopkg.in/urfave/cli.v1"
+	"fmt"
+	"os"
+	"math/rand"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	app.Name = "Lorem Ipsum Generator"
+
+	app.Flags = []cli.Flag{
+		cli.Float64Flag{Name: "rate", Usage: "Lines of lorem ipsum per second", Value: 1,},
+		cli.IntFlag{Name: "min-words", Usage: "Minimum number of words per line", Value: 10,},
+		cli.IntFlag{Name: "max-words", Usage: "Maximum number of words per line", Value: 10,},
+		cli.Int64Flag{Name: "rand-seed", Usage: "Specify the random seed of the generator.", Value: 0,},
+
+	}
+
+	app.Action = func(c *cli.Context) error {
+		rand.Seed(c.Int64("rand-seed"))
+
+		interval := float64(1) / c.Float64("rate")
+
+		ticker := time.Tick(time.Second * time.Duration(interval))
+
+		for _ = range ticker {
+			fmt.Println(lorem.Sentence(c.Int("min-words"), c.Int("max-words")))
+		}
+
+		return nil
+	}
+
+	app.Run(os.Args)
+}
